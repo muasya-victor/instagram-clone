ec76602c224f6bb2962fe838bfe53cbdb9e7bb97
victor muasya <vicmwe184@gmail.com>
2022-06-08 07:39:36 +0300

    hurry

--- src/views/PostView.vue
+++ src/views/PostView.vue
@@ -1,15 +1,24 @@
 <template>
+  <div class="relative top-1/2 left-0 sticky">
+    <postssvg/>
+  </div>
+
   <div class="" v-for="(post, index) in $store.state.posts" :key="index">
     <Post :url="post.url" :name="post.name" :user_url="post.user_url"/>
   </div>
+
+
 </template>
 
 <script>
 import Post from "@/components/Post";
+import postssvg from "@/components/svgs/postssvg";
+
 export default {
   name: "PostView",
   components :{
-    Post
+    Post,
+    postssvg
   }
 }
 </script>
